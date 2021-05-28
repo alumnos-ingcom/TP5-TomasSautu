9337861b1d1f02d1e1132f27834e8dd85b480111
TomasSautu <tomassautu@gmail.com>
2021-05-28 19:04:46 -0300

    Ejercicio 6 terminado! (Paréntesis Balanceado) (cambiado)
    Cambia todo el código para que solo exista una función (balanceado() ) en el que se analizan los 3 caracteres (paréntesis, llaves, corchetes) y de una respuesta de cada uno.

--- tp5ej6.py
+++ tp5ej6.py
@@ -3,68 +3,61 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-def parentesis_balanceado(texto):
+def balanceado(texto):
     parentesis_abierto = 0
     parentesis_cerrado = 0
-    for i in range(len(texto)):
-        if (ord(texto[i]) == 40):
-            parentesis_abierto += 1
-        elif (ord(texto[i]) == 41):
-            parentesis_cerrado += 1
-    if (parentesis_abierto + parentesis_cerrado) == 0:
-        return "No Tiene"
-    else:
-        
-        if parentesis_abierto == parentesis_cerrado:
-            return "OK"
-        else:
-            return "NO OK"
-
-def llaves_balanceadas(texto):
     llave_abierta = 0
     llave_cerrada = 0
-    for i in range(len(texto)):
-        if (ord(texto[i]) == 123):
-            llave_abierta += 1
-        elif (ord(texto[i]) == 125):
-            llave_cerrada += 1
-    if (llave_abierta + llave_cerrada)==0:
-        return "No Tiene"
-    else:
-        
-        if llave_abierta == llave_cerrada:
-            return "OK"
-        else:
-            return "NO OK"
-
-def corchetes_balanceados(texto):
     corchete_abierto = 0
     corchete_cerrado = 0
     for i in range(len(texto)):
-        if (ord(texto[i]) == 91):
+        if (texto[i] == "("):
+            parentesis_abierto += 1
+        elif (texto[i] == ")"):
+            parentesis_cerrado += 1
+        elif (texto[i] == "{"):
+            llave_abierta += 1
+        elif (texto[i] == "}"):
+            llave_cerrada += 1
+        elif (texto[i] == "["):
             corchete_abierto += 1
-        elif (ord(texto[i]) == 93):
+        elif (texto[i] == "]"):
             corchete_cerrado += 1
+    if (parentesis_abierto + parentesis_cerrado) == 0:
+        parentesis = ("No Tiene")
+    else:
+        
+        if parentesis_abierto == parentesis_cerrado:
+            parentesis = ("OK")
+        else:
+            parentesis = ("NO OK")
+        
+    if (llave_abierta + llave_cerrada)==0:
+        llaves = ("No Tiene")
+    else:
+        
+        if llave_abierta == llave_cerrada:
+            llaves = ("OK")
+        else:
+            llaves = ("NO OK")
     if (corchete_abierto + corchete_cerrado) == 0:
-        return "No Tiene"
+        corchetes = ("No Tiene")
     else:
         
         if corchete_abierto == corchete_cerrado:
-            return "OK"
+            corchetes = ("OK")
         else:
-            return "NO OK"
-
+            corchetes = ("NO OK")
+    return parentesis,llaves, corchetes
 
 def prueba():
     print("""Este programa analizara un texto y le dira si los paréntesis, llaves o corchetes estan balanceados
 (balanceado se refiere a que cada uno tiene su caracter de apertura y de cierre)""")
     texto = input("Ingrese el texto: ")
-    respuesta_parentesis = parentesis_balanceado(texto)
-    respuesta_llaves = llaves_balanceadas(texto)
-    respuesta_corchetes = corchetes_balanceados(texto)
-    print(f"Paréntesis: {respuesta_parentesis}")
-    print(f"Llaves: {respuesta_llaves}")
-    print(f"Corchetes: {respuesta_corchetes}")
+    parentesis, llaves, corchetes = balanceado(texto)
+    print(f"Paréntesis: {parentesis}")
+    print(f"Llaves: {llaves}")
+    print(f"Corchetes: {corchetes}")
 
 if __name__ == "__main__":
     prueba()
