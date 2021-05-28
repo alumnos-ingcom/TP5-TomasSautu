0c2a3ee1bf4fff99df03971b17cb279dc7f8b4ca
TomasSautu <tomassautu@gmail.com>
2021-05-28 18:49:50 -0300

    Ejercicio 3 terminado! (Tribonacci) (corregido)
    Cambie detalles en el return para que devuelva en forma de tupla y "saque" los argumentos "termino_x" y los puse en el código adentro de la función.

--- tp5ej3.py
+++ tp5ej3.py
@@ -5,14 +5,17 @@
 
 from utilidades import ingreso_entero
 
-def tribonacci(numero, termino_1 = 1, termino_2 = 1, termino_3 = 1):
+def tribonacci(numero):
+    termino_1 = 1
+    termino_2 = 1
+    termino_3 = 1
     lista = [termino_1, termino_2, termino_3]
     n_esimo = numero - 2
     for i in range(n_esimo-1):
         lista.append(lista[-1]+lista[-2]+lista[-3])
         resultado = lista[-1]
         
-    return lista, resultado
+    return (lista, resultado)
 
 
 
@@ -23,7 +26,7 @@ El número debe ser un entero positivo mayor a 3.
     numero = ingreso_entero("Ingrese un número para calcular su n-esimo termino de Fibonacci: ")
     if numero > 3:
         lista, resultado = tribonacci(numero)
-        print(f"El resultado es {resultado} la sucesión es {lista}")
+        print(f"El resultado es {resultado}, la sucesión es {lista}")
     else:
         print(f"El número {numero} no es mayor a 3.")
 
