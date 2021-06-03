6b61d8728588a463b5cab2832e8dc2728946c169
TomasSautu <tomassautu@gmail.com>
2021-06-03 16:23:23 -0300

    Ejercicio 10 terminado! (Texto Binario)
    En este programa se pide el ingreso de un número entero positivo, si el número entero no es positivo se avisará y terminará la ejecución. Si el número esta dentro de los positivos este entra a la función numero_binario(). En esta función se hace un cálculo del módulo del número ingreso, el resultado se guarda en una lista, luego se divide ese número que ingreso el usuario por 2, el resultado vuelve a pasar por el mismo loop de calcular el módulo del número y guardarlo en la lista. Una vez el número es igual a 0 la lista invertida con los resultados se convierte a un texto, este texto se retorna para luego imprimir una respuesta.

---
+++ tp5ej10.py
@@ -0,0 +1,31 @@
+################
+# Tomás Sautú - @TomasSautu
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from utilidades import ingreso_entero
+
+def numero_binario(numero):
+    binario = []
+    if numero > 0:
+        while (numero > 0 or numero >= 1):
+            num_binario = numero % 2
+            text_binario = str(num_binario)
+            binario.append(text_binario)
+            numero = numero // 2
+    else:
+        binario.append("0")
+    binario = "".join(binario[::-1])
+    return binario
+
+
+def prueba():
+    numero = ingreso_entero("Ingrese un número entero positivo para convertirlo en binario: ")
+    if numero >= 0:
+        binario = numero_binario(numero)
+        print(f"El número entero {numero} convertido en binario es: {binario}")
+    else:
+        print("El número tiene que ser un entero positivo")
+
+if __name__ == "__main__":
+    prueba()
