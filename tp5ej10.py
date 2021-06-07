2745335879eb61544e1e3cef4921fac6134428ff
TomasSautu <tomassautu@gmail.com>
2021-06-07 15:13:21 -0300

    Ejercicio 10 terminado! (Texto Binario)
    Le agregue la función para hacer el proceso inverso de la función numero_binario, convertir un texto binario a un número entero. El proceso se hace mediante potencias de 2 para calcular el número. según la cantidad de dígitos en el número binario sera la cantidad de potencias. Solo se suman los números resultantes de las potencias de dos que provengan del dígito 1 del número binario.

--- tp5ej10.py
+++ tp5ej10.py
@@ -18,14 +18,38 @@ def numero_binario(numero):
     binario = "".join(binario[::-1])
     return binario
 
+def binario_numero(binario):
+    cifras = []
+    binarioTxt = str(binario)
+    suma = 0
+    for i in binarioTxt:
+        cifras.append(i)
+    cifras.reverse()
+    for i in range(len(cifras)):
+        if cifras[i] == '1':
+            suma += 2**i
+    return suma
 
 def prueba():
-    numero = ingreso_entero("Ingrese un número entero positivo para convertirlo en binario: ")
-    if numero >= 0:
-        binario = numero_binario(numero)
-        print(f"El número entero {numero} convertido en binario es: {binario}")
+    funcion = ingreso_entero(""""Si quiere convertir un número entero positivo a binario ingrese 1
+Si quiere convertir un binario a número entero ingrese 2
+
+#""")
+    if funcion == 1 or funcion == 2:
+        if funcion == 1:
+            numero = ingreso_entero("Ingrese un número entero positivo para convertirlo en binario: ")
+            if numero >= 0:
+                binario = numero_binario(numero)
+                print(f"El número entero {numero} convertido en binario es: {binario}")
+            else:
+                print("El número tiene que ser un entero positivo")
+        else:
+            binario = ingreso_entero("Ingrese el número binario para convertirlo a número entero: ")
+            numero = binario_numero(binario)
+            print(f"El número binario {binario} convertido es {numero}")
+            
     else:
-        print("El número tiene que ser un entero positivo")
+        print("No es una opción para el programa")
 
 if __name__ == "__main__":
     prueba()
